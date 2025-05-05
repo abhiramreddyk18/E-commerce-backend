1cac7012e13a504ba79f7f2a50276b32f3564732
abhiram <abhiramreddyk18gmail.com>
2025-05-05 21:25:58 +0530

    changed cors policy

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectDB();
 app.use(express.json());
 
 app.use(cors({
-    origin: ["http://localhost:4200", process.env.FRONTEND_URL],
+    origin: ["http://localhost:4200", "https://abhiramreddyk18.github.io/E-commerce-frontend"],
     credentials: true,
 }));
 
